ebe0a3c2697adc3d359178d3ea208279f01ebc1d
Adam Demuri <adam.demuri@gmail.com>
2019-12-07 22:56:02 -0700

    Allow changing number of poles for rotate three

--- generic/RotateThreeEffect.cpp
+++ generic/RotateThreeEffect.cpp
@@ -9,14 +9,14 @@ void RotateThreeEffect::DoRun() {
   static uint8_t pole_offset = 0;
   static int8_t light_offset = 0;
 
-  for (int pole = 0; pole < 3; pole++) {
+  for (int pole = 0; pole < num_poles_on; pole++) {
     for (int i = 0; i < 4; i++) {
       if (mode / 2 == 0) {
-        poles[pole * 2 + pole_offset]->SetGridLight(light_offset, i,
-                                                    CRGB(0, 0, 0));
+        poles[pole * (poles.size() / num_poles_on) + pole_offset]->SetGridLight(
+            light_offset, i, CRGB(0, 0, 0));
       } else {
-        poles[pole * 2 + pole_offset]->SetGridLight(i, light_offset,
-                                                    CRGB(0, 0, 0));
+        poles[pole * (poles.size() / num_poles_on) + pole_offset]->SetGridLight(
+            i, light_offset, CRGB(0, 0, 0));
       }
     }
   }
@@ -38,22 +38,22 @@ void RotateThreeEffect::DoRun() {
     } else {
       light_offset = 3;
     }
-    pole_offset = (pole_offset + 1) % 2;
+    pole_offset = (pole_offset + 1) % (poles.size() / num_poles_on);
   }
   if (light_offset < 0) {
     light_offset = 0;
-    pole_offset = (pole_offset + 1) % 2;
+    pole_offset = (pole_offset + 1) % (poles.size() / num_poles_on);
   }
   hue_base += 10;
 
-  for (int pole = 0; pole < 3; pole++) {
+  for (int pole = 0; pole < num_poles_on; pole++) {
     for (int i = 0; i < 4; i++) {
       if (mode / 2 == 0) {
-        poles[pole * 2 + pole_offset]->SetGridLight(
+        poles[pole * (poles.size() / num_poles_on) + pole_offset]->SetGridLight(
             light_offset, i,
             CHSV(hue_base + 255 / 3 * pole + i * 12, 255, 255));
       } else {
-        poles[pole * 2 + pole_offset]->SetGridLight(
+        poles[pole * (poles.size() / num_poles_on) + pole_offset]->SetGridLight(
             i, light_offset,
             CHSV(hue_base + 255 / 3 * pole + i * 12, 255, 255));
       }
--- generic/RotateThreeEffect.hpp
+++ generic/RotateThreeEffect.hpp
@@ -16,7 +16,9 @@ class RotateThreeEffect : public Effect {
   uint32_t timer = 0;
   // 0 to 3, controls up-down or left-right and same direction or alternating
   // directions.
-  uint8_t mode = 3;
+  uint8_t mode = 2;
+
+  uint8_t num_poles_on = 2;
 };
 
 #endif
