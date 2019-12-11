88aace74c1ac1298b714bfe5df468a649078fb60
Adam Demuri <adam.demuri@gmail.com>
2019-12-10 21:08:31 -0700

    Enable temporal dithering for FastLED

--- arduino/direct/direct.ino
+++ arduino/direct/direct.ino
@@ -15,14 +15,18 @@ void setup() {
   Serial.begin(115200);
 
   light_controller = new FastLedLightController();
-  //effect = new BrightDarkEffect(light_controller->get_poles()); 
+  effect = new BrightDarkEffect(light_controller->get_poles()); 
   //effect = new ColorCycleEffect(light_controller->get_poles()); 
   //effect = new CrossEffect(light_controller->get_poles()); 
-  effect = new MiniCircleEffect(light_controller->get_poles()); 
+  //effect = new MiniCircleEffect(light_controller->get_poles()); 
   //effect = new RotateThreeEffect(light_controller->get_poles()); 
 }
 
 void loop() {
   effect->Run();
   light_controller->WriteOutLights();
+
+  // This gives FastLED time to do temporal dithering (smoothing out low
+  // values).
+  FastLED.delay(1);
 }
