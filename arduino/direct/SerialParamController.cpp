a280b35ed98026543706934e2a8632ffded720f0
Samantha Piccone <piccones@gmail.com>
2021-04-02 12:41:16 -0600

    Update controlled effects. In progress!

--- arduino/direct/SerialParamController.cpp
+++ arduino/direct/SerialParamController.cpp
@@ -10,48 +10,17 @@ void SerialParamController::Tick() {
   if (Serial.available() > 0) {
     String in = Serial.readStringUntil('\n');
     switch (in.charAt(0)) {
-      case 'p':
-        params[static_cast<uint8_t>(Param::kActivePoles)] =
+      case 'a':
+        params[static_cast<uint8_t>(Param::kBeatReset)] =
             in.substring(2).toInt();
         break;
 
-      case 'm':
-        params[static_cast<uint8_t>(Param::kMovementMode)] =
-            in.substring(2).toInt();
-        break;
-
-      case 's':
-        params[static_cast<uint8_t>(Param::kMovementSpeed)] =
-            in.substring(2).toInt();
-        break;
-
-      case 'l':
-        params[static_cast<uint8_t>(Param::kBalanceColor)] =
-            in.substring(2).toInt();
-        break;
-
-      case 'h':
-        params[static_cast<uint8_t>(Param::kPollHueShift)] =
-            in.substring(2).toInt();
-        break;
-
-      case 'g':
-        params[static_cast<uint8_t>(Param::kGridMode)] =
-            in.substring(2).toInt();
+      case 'b':
+        params[static_cast<uint8_t>(Param::kBpm)] = in.substring(2).toInt();
         break;
 
       case 'c':
         params[static_cast<uint8_t>(Param::kGridLightCount)] =
-            in.substring(2).toInt();
-        break;
-
-      case 'e':
-        params[static_cast<uint8_t>(Param::kGridSpeed)] =
-            in.substring(2).toInt();
-        break;
-
-      case 'i':
-        params[static_cast<uint8_t>(Param::kGridHueShift)] =
             in.substring(2).toInt();
         break;
 
@@ -60,8 +29,38 @@ void SerialParamController::Tick() {
             in.substring(2).toInt();
         break;
 
-      case 'b':
+      case 'e':
+        params[static_cast<uint8_t>(Param::kGridSpeed)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'f':
         params[static_cast<uint8_t>(Param::kGridBackForth)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'g':
+        params[static_cast<uint8_t>(Param::kGridMode)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'h':
+        params[static_cast<uint8_t>(Param::kPollHueShift)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'i':
+        params[static_cast<uint8_t>(Param::kGridHueShift)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'l':
+        params[static_cast<uint8_t>(Param::kBalanceColor)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'm':
+        params[static_cast<uint8_t>(Param::kMovementMode)] =
             in.substring(2).toInt();
         break;
 
@@ -70,9 +69,23 @@ void SerialParamController::Tick() {
             in.substring(2).toInt();
         break;
 
+      case 'p':
+        params[static_cast<uint8_t>(Param::kActivePoles)] =
+            in.substring(2).toInt();
+        break;
+
       case 'r':
         params[static_cast<uint8_t>(Param::kGridReverse)] =
             in.substring(2).toInt();
+        break;
+
+      case 's':
+        params[static_cast<uint8_t>(Param::kMovementSpeed)] =
+            in.substring(2).toInt();
+        break;
+
+      case 'u':
+        params[static_cast<uint8_t>(Param::kPause)] = in.substring(2).toInt();
         break;
 
       default:
