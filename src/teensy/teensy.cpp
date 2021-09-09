fd680a45c32d6ebc2289cd8a8f6fc8bba2d0a43e
Adam Demuri <adam.demuri@gmail.com>
2021-09-09 09:54:12 -0600

    Seed random using Entropy library

--- src/teensy/teensy.cpp
+++ src/teensy/teensy.cpp
@@ -1,3 +1,5 @@
+#include <Entropy.h>
+
 #include <ColordanceTypes.hpp>
 #include <DemoController.hpp>
 
@@ -14,6 +16,10 @@ void setup() {
   param_controller = new LocalButtonController();
   light_controller = new TeensyLightController();
   effect = new DemoController(light_controller->get_poles(), param_controller);
+
+  // See https://forum.pjrc.com/threads/61974-Teensy4-x-Entropy-library-documentation
+  Entropy.Initialize();
+  randomSeed(Entropy.random(0, 32767));
 }
 
 void loop() {
