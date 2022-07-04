1b3daac16cef2905428e1d6c3220d7891451ad4e
David Stone <davidfromonline@gmail.com>
2022-07-04 02:20:30 -0600

    Some versions of compilers we use do not support `inline` variables

--- lib/generic/Grid.hpp
+++ lib/generic/Grid.hpp
@@ -3,8 +3,8 @@
 #include <array>
 #include <cstdint>
 
-inline constexpr std::uint8_t gridWidth = 4;
-inline constexpr std::uint8_t gridHeight = 4;
+constexpr std::uint8_t gridWidth = 4;
+constexpr std::uint8_t gridHeight = 4;
 
 template<typename T>
 using Grid = std::array<std::array<T, gridWidth>, gridHeight>;
