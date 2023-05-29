79aa36bf0f6f5854e572886825dfdbe104b06250
Adam Demuri <adam.demuri@gmail.com>
2023-05-28 19:39:46 -0600

    WIP debugging changes

--- lib/teensy-sd-logger/TeensySdLogger.cpp
+++ lib/teensy-sd-logger/TeensySdLogger.cpp
@@ -4,42 +4,42 @@ TeensySdLogger::TeensySdLogger(
     const ParamController& param_controller,
     const EnvironmentController& environment_controller)
     : Logger(param_controller, environment_controller) {
-  if (!SD.begin(BUILTIN_SDCARD)) {
-    Serial.println("SD card begin() failed");
-    sd_init_ = false;
-    return;
-  }
-  Serial.println("SD card begin() success");
-  sd_init_ = true;
+  // if (!SD.begin(BUILTIN_SDCARD)) {
+  //   Serial.println("SD card begin() failed");
+  //   sd_init_ = false;
+  //   return;
+  // }
+  // Serial.println("SD card begin() success");
+  // sd_init_ = true;
 
-  const size_t name_size = 100;
-  char name[name_size] = "";
-  int n = -1;
-  for (n = 0; n < 10000; n++) {
-    int ret = snprintf(name, name_size, "colordance_log_%d.csv", n);
-    if (ret < 0 || ret >= (int)name_size) {
-      Serial.print("Error formatting log file name: ");
-      Serial.println(ret);
-      sd_init_ = false;
-      return;
-    }
-    if (!SD.exists(name)) {
-      break;
-    }
-  }
-  if (n < 0) {
-    Serial.println("All log files occupied, not logging");
-    sd_init_ = false;
-    return;
-  }
+  // const size_t name_size = 100;
+  // char name[name_size] = "";
+  // int n = -1;
+  // for (n = 0; n < 10000; n++) {
+  //   int ret = snprintf(name, name_size, "colordance_log_%d.csv", n);
+  //   if (ret < 0 || ret >= (int)name_size) {
+  //     Serial.print("Error formatting log file name: ");
+  //     Serial.println(ret);
+  //     sd_init_ = false;
+  //     return;
+  //   }
+  //   if (!SD.exists(name)) {
+  //     break;
+  //   }
+  // }
+  // if (n < 0) {
+  //   Serial.println("All log files occupied, not logging");
+  //   sd_init_ = false;
+  //   return;
+  // }
 
-  Serial.print("Logging to: ");
-  Serial.println(name);
-  log_file_ = SD.open(name, FILE_WRITE);
-  if (!log_file_) {
-    Serial.println("Error opening log file");
-  }
-  sd_flush_timer_.Reset();
+  // Serial.print("Logging to: ");
+  // Serial.println(name);
+  // log_file_ = SD.open(name, FILE_WRITE);
+  // if (!log_file_) {
+  //   Serial.println("Error opening log file");
+  // }
+  // sd_flush_timer_.Reset();
 }
 
 void TeensySdLogger::Log(const char* message) {
