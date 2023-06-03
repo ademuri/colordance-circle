99da596b6f70f0615a2bad903d541deba7714f3f
Adam Demuri <adam.demuri@gmail.com>
2023-06-03 15:43:35 -0600

    Fix moving pole

--- lib/generic/interface/effects/MovingPole.hpp
+++ lib/generic/interface/effects/MovingPole.hpp
@@ -45,11 +45,10 @@ class MovingPole : public InterfaceEffect {
   uint8_t baseHue = 0;
   uint8_t hueDistance = 42;
 
-  Mode modes[5] = {Mode::kLine, Mode::kLine, Mode::kCircle, Mode::kPinwheel,
-                   Mode::kSmallSquare};
+  Mode modes[5] = {Mode::kLine, Mode::kLine, Mode::kCircle, Mode::kSmallSquare};
   Speed speeds[5] = {Speed::kDefault, Speed::kStill, Speed::kDefault,
                      Speed::kDefault, Speed::kDefault};
-  uint8_t rotations[5] = {0, 2, 0, 0, 0};
+  uint8_t rotations[5] = {0, 2, 0, 0};
   uint8_t modeIndex = 0;
   const uint8_t kNumModes = 5;
 
